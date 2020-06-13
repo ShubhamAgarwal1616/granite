2b5c88e97d884d4568594ebd50818d9dd0aa5cc7
Shubham Agarwal <shubham.agarwal@navi.com>
2020-06-13 11:46:36 +0530

    Added ability to update a task

--- app/javascript/components/tasks/Edit.js
+++ app/javascript/components/tasks/Edit.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
 
+import { fetchApi } from '../../utils/API';
+import * as Routes from '../../utils/Routes';
+import Errors from '../shared/Errors';
+
 class Edit extends Component {
   constructor(props) {
     super(props);
     this.state = {
       description: this.props.task.description,
+      errors: null,
     };
+    this.handleError = this.handleError.bind(this);
+  }
+
+  handleError(response) {
+    this.setState({
+      errors: {
+        errors: response.messages,
+        type: response.type,
+      },
+    });
   }
 
   handleChange = e => {
     this.setState({ description: e.target.value });
   };
 
-  displayEditTaskForm() {
+  handleSubmit = e => {
+    e.preventDefault();
+    fetchApi({
+      url: Routes.task_path(this.props.task.id),
+      method: 'PATCH',
+      body: { description: this.state.description },
+      onError: this.handleError,
+      onSuccess: response => {
+        console.log(response);
+      },
+      successCallBack: () => {
+        window.location.replace(Routes.tasks_path());
+      },
+    });
+  };
+
+  displayErrors() {
+    const { errors } = this.state;
+
+    return (
+      <div className="row">
+        {errors && (
+          <div className="mt-4">
+            <Errors errors={errors.errors} message={errors.type} />
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  displayNewTaskForm() {
     return (
       <div>
         <div className="form-row">
@@ -29,7 +74,11 @@ class Edit extends Component {
           </div>
         </div>
         <br />
-        <button className="btn btn-md btn-primary" type="submit">
+        <button
+          className="btn btn-md btn-primary"
+          type="submit"
+          onClick={this.handleSubmit}
+        >
           Update task
         </button>
       </div>
@@ -41,7 +90,8 @@ class Edit extends Component {
       <React.Fragment>
         <div className="container">
           <h3 className="py-3">Enter new task details</h3>
-          {this.displayEditTaskForm()}
+          {this.displayErrors()}
+          {this.displayNewTaskForm()}
         </div>
       </React.Fragment>
     );
--- app/javascript/components/tasks/New.js
+++ app/javascript/components/tasks/New.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import API from '../../utils/API';
 import * as Routes from '../../utils/Routes';
 import Errors from '../shared/Errors';
+import { fetchApi } from '../../utils/API';
 
 class New extends Component {
   constructor(props) {
@@ -14,6 +15,10 @@ class New extends Component {
 
     this.onSubmit = this.onSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleError(response) {
+    this.setState({errors: response.messages});
   }
 
   displayErrors() {
@@ -32,18 +37,22 @@ class New extends Component {
 
   onSubmit(event) {
     event.preventDefault();
-    API.postNewTask({ task: { description: this.state.description } })
-      .then((response) => {
-        this.setState({message: response.notice})
-        setTimeout(function(){
-          window.location.href = Routes.task_path(response.id);
-        }, 1000)
-      })
-      .catch(error => {
-        error.json().then(({ errors }) => {
-          this.setState({...this.state, errors})
-        });
-      });
+    fetchApi({
+      url: Routes.tasks_path(),
+      method: 'POST',
+      body: {
+        task: { description: this.state.description },
+      },
+      onError: this.handleError,
+      onSuccess: response => {
+        this.setState({ message: response.messages[0] });
+      },
+      successCallBack: response => {
+        setTimeout(function () {
+          window.location.replace(Routes.task_path(response.id));
+        }, 1000);
+      },
+    });
   }
 
   handleChange(event) {
