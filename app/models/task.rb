58d4013a656865db1867441e02b0b2028ffd8db2
Shubham Agarwal <shubham.agarwal@navi.com>
2020-06-13 12:21:23 +0530

    Added ability to assign task to a user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   validates :description, presence: true
+  belongs_to :user, optional: true
+  before_validation :assign_description, unless: :description_present
+
+  def assign_description
+    self.description = 'add description to task '
+  end
+
+  def description_present
+    self.description.present?
+  end
 end
